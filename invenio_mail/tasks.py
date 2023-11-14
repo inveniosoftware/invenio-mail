8eb4a4197608129a5df6ffcd08587791c4779428
gressho <gressho@uni-muenster.de>
2023-11-14 11:41:20 +0100

    Go the other way round to handle the attachments.

--- invenio_mail/tasks.py
+++ invenio_mail/tasks.py
@@ -36,39 +36,11 @@ def send_email(data):
        large to handle by the messaging queue, so check for a maximum size beforehand.
     """
     msg = Message()
-    msg.subject = data.get("subject")
-    if "html" in data:
-        msg.html = data.get("html")
+    attachments = data.pop("attachments", None)
+    msg.__dict__.update(data)
 
-    msg.body = data.get("body")
-    msg.recipients = data.get("recipients")
-    msg.sender = data.get("sender")
-    if "reply_to" in data:
-        msg.reply_to = data.get("reply_to")
-
-    if "date" in data:
-        msg.date = data.get("date")
-
-    if "cc" in data:
-        msg.cc = data.get("cc")
-
-    if "bcc" in data:
-        msg.cc = data.get("bcc")
-
-    if "charset" in data:
-        msg.cc = data.get("charset")
-
-    if "extra_headers" in data:
-        msg.cc = data.get("extra_headers")
-
-    if "mail_options" in data:
-        msg.cc = data.get("mail_options")
-
-    if "rcpt_options" in data:
-        msg.cc = data.get("rcpt_options")
-
-    if "attachments" in data:
-        for attachment in data.get("attachments"):
+    if attachments is not None:
+        for attachment in attachments:
             rawdata = b64decode(attachment.get("base64"))
             content_type = "application/octet-stream"
             if "content_type" in attachment:
